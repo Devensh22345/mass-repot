1df645c8e8cee17cd611f20d0ddf64a0072e73f5
Devensh22345 <kumardevensh0@gmail.com>
2025-08-31 02:20:06 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -22,7 +22,7 @@ class Config:
     SUDO = list(map(int, os.getenv("SUDO", "6872968794").split()))
 
     # MongoDB Configuration
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Test:Test@cluster0.pcpx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mass:mass@cluster0.4nrgvz8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "Test")
     # Log Channel (Add your log channel ID here)
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002372592189"))  # Default is a placeholder
